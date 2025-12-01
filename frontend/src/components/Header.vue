dc7ff9141e69beb6238c5f8376394a471b9453a6
Unvoniddin Gulmirzoev <234453234+dmnzeal@users.noreply.github.com>
2025-12-02 00:36:11 +0500

    feat: add header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="logo">
+        <router-link class="link" to="/">InShort</router-link>
+      </div>
+      <nav class="menu">
+        <!-- <router-link class="link" to="/dashboard">Главная</router-link> -->
+        <router-link class="link" to="/login">Вход</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: #fafafa;
+  border-bottom: 2px solid #f0f0f0;
+}
+.container {
+  max-width: 1024px;
+  height: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1rem;
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.menu {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 1rem;
+}
+.link {
+  text-decoration: none;
+  color: #353839;
+}
+</style>
