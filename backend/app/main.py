6e25a908b752cd10b124a693eecd507dad6d9168
Unvoniddin Gulmirzoev <234453234+dmnzeal@users.noreply.github.com>
2025-12-01 19:33:29 +0500

    feat: add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import create_db_and_tables
 from app.routers import url_shortener, qr_generator, users
@@ -12,6 +13,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=url_shortener.router)
 app.include_router(router=qr_generator.router)
